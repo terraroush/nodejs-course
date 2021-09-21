8a6e784d248d817355eebbe148bd5b20baea8e27
Terra Roush <terra.roush@gmail.com>
2021-09-21 18:22:18 -0500

    learning Express with dynamic values using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,64 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static deirectory to serve
+app.use(express.static(publicDirPath))
+
 app.get('', (req, res) => {
-    res.send("Hello express!")
+    res.render("index", {
+        title: "Weather",
+        name: "Terra Roush"
+    })
 })
-
-app.get('/help', (req, res) => {
-    res.send("Help Page")
-})
-
 app.get('/about', (req, res) => {
-    res.send("About Page")
+    res.render("about", {
+        title: "About Me",
+        name: "Terra Roush"
+    })
+})
+app.get('/help', (req, res) => {
+    res.render("help", {
+        title: "Help",
+        helpText: "This is helpful text!",
+        name: "Terra Roush"
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send("Show Weather")
+    res.send({
+        forecast: "It's 78 degrees.",
+        location: "Denver"
+    })
+})
+
+
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        errorMessage: "Help article not found.",
+        name: "Terra Roush"
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        errorMessage: "Page not found.",
+        name: "Terra Roush"
+    })
 })
 
 app.listen(3000, () => {
