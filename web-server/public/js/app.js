291da0d77095f0a32062db75428948e7a41abef2
Terra Roush <terra.roush@gmail.com>
2021-09-22 14:38:39 -0500

    completed fetch call to get static weather data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log("client side js file is loaded")
+
+fetch('http://localhost:3000/weather?city=boston').then((response) => {
+    response.json().then(data => {
+        if(data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
