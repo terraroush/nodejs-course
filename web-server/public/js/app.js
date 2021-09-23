f29b40784029d2f6f06abb8e606972cc89ce1985
Terra Roush <terra.roush@gmail.com>
2021-09-23 13:25:03 -0500

    added form to the weather homepage, connected fetch to the submit event

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,18 @@
-console.log("client side js file is loaded")
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?city=boston').then((response) => {
-    response.json().then(data => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    fetch(`http://localhost:3000/weather?city=${location}`).then((response) => {
+        response.json().then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
