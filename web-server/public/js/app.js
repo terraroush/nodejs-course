09407739d007aa6cf6bd50fff87ebf816cfdec55
Terra Roush <terra.roush@gmail.com>
2021-09-23 14:59:24 -0500

    adjusted message paragraph to clear when appropriate

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,25 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#messageOne')
+const messageTwo = document.querySelector('#messageTwo')
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+
+    messageOne.textContent = 'loading...'
+    messageTwo.textContent = ''
+    
     fetch(`http://localhost:3000/weather?city=${location}`).then((response) => {
         response.json().then(data => {
-            if(data.error) {
-                console.log(data.error)
+            if (data.error) {
+                messageOne.textContent = ''
+                messageTwo.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
